d514e78680603f19af54263a24abae98d52b40f7
Wellington dos Santos Castor <wellington@wton.com.br>
2022-04-08 12:03:20 -0300

    update

--- Fundamentos Projeto Twitter/src/twitter/TwetterAuth.java
+++ Fundamentos Projeto Twitter/src/twitter/TwetterAuth.java
@@ -3,19 +3,19 @@ package twitter;
 public enum TwetterAuth {
 	
 	/* Insert here your personal "Consumer Key (API Key)" from Twitter */
-	API_KEY("Rn94c8CiHCP8P9UczuBtP8tpH"),
+	API_KEY(""),
 	/* Insert here your personal "Consumer Secret (API Secret)" from Twitter */
-	API_SECRET("jas6SHevcs09X1iJW3c7NKxlAj9bxNEecgtfKt7MwmIkhCNdLa"),
+	API_SECRET(""),
 	/* Insert here your personal "Access Token" from Twitter */
-	ACCESS_TOKEN("128530922-83WhDPGxFEhYOdJaeq0XEwNBrebWSvGSgBwPBxio"),
+	ACCESS_TOKEN(""),
 	/* Insert here your personal "Access Token Secret" from Twitter */
-	ACCESS_TOKEN_SECRET("XZrbahYRllt5gSev32kEaER0xJBqLFuHgf5dCLBuB4R6A"),
+	ACCESS_TOKEN_SECRET(""),
 	
 	
 	/* Insert here your name "Owner"  from Twitter */
 	OWNER("wtondossantos"),
 	/* Insert here your "Owner ID" from Twitter */
-	OWNER_ID("128530922");
+	OWNER_ID("");
 	
 	private String key;
 
